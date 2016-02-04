08c9eec7c978b37cbcaee8fb025656b1821d69df
Jon Badalamenti <jonb@umn.edu>
2016-02-04 15:22:08 -0600

    added statistics for TA insertion sites

--- insertion_statistics_by_locustag.py
+++ insertion_statistics_by_locustag.py
@@ -3,9 +3,11 @@
 import sys
 import os
 import csv
+import re
 import Bio
 from Bio import SeqIO, SeqFeature
 from Bio.SeqRecord import SeqRecord
+from Bio.Seq import Seq
 import argparse
 from argparse import RawTextHelpFormatter
 
@@ -18,11 +20,11 @@ warnings.simplefilter('ignore', BiopythonWarning)
 
 # script help and usage
 parser=argparse.ArgumentParser(
-    description='Given a tab-delimited file providing genomic coordinates of transposon insertions, this script\nparses a Genbank file and tabulates the total number of transposon insertions and insertion sites\nper locus tag. It also tabulates intergenic insertions and locus tags without any insertions. \n\nNOTE: Organisms with multiple Genbank records (e.g. those with multiple chromosomes or plasmids)\nshould be concatenated into a single .gbk file before executing this script. For example:\n% cat NC_000001.gbk NC_000002.gbk [...NC_00000n.gbk] > concatenated.gbk\n\nRequires BioPython v. 1.65 or later (http://biopython.org/wiki/Download)', 
+    description='Given 1) a tab-delimited file providing genomic coordinates of transposon insertions and\n2) a locus tag of interest, this script parses a Genbank file and tabulates the total number of\ntransposoninsertions for each insertion site within the genomic coordinates of the given locus tag.\n\nNOTE: Organisms with multiple Genbank records (e.g. those with multiple chromosomes or plasmids)\nshould be concatenated into a single .gbk file before executing this script. For example:\n% cat NC_000001.gbk NC_000002.gbk [...NC_00000n.gbk] > concatenated.gbk\n\nRequires BioPython v. 1.65 or later (http://biopython.org/wiki/Download)', 
     epilog='Author: Jon Badalamenti, Bond Lab, University of Minnesota (http://www.thebondlab.org)\nhttp://github.com/jbadomics/tnseq\nFebruary 2016\n \n', formatter_class=RawTextHelpFormatter)
 parser.add_argument('[GENBANK FILE]', help='Genbank file containing, at a minimum, locus tags and corresponding genomic\ncoordinates')
 parser.add_argument('[DATA FILE]', help='insertion data as three-column tab-delimited file containing chromosome,\ngenomic coordinate, and total number of insertions at that coordinate, e.g.\nchr1\t327491\t1639')
-parser.add_argument('[LOCUS TAG]', help='the locus tag for which to report insertion sites; output file names will be prepended with the locus tag as a prefix')
+parser.add_argument('[LOCUS TAG]', help='the locus tag for which to report insertion sites; output files will be named\n<locus tag>.insertions.txt')
 parser.add_argument('[N-terminal trim]', type=float, help='percent (expressed as a decimal) of the total gene length to trim from the\nN-terminus of the translated protein')
 parser.add_argument('[C-terminal trim]', type=float, help='percent (expressed as a decimal) of the total gene length to trim from the\nC-terminus of the translated protein')
 args=parser.parse_args()
@@ -39,18 +41,27 @@ with open(sys.argv[2], 'r') as f:
 	for chromosome,coordinate,insertion in insertionPointsFile:
 		insertionEvents.append([chromosome, coordinate, insertion])
 
-outputFileName = "%s.insertions.txt" % sys.argv[3]
-outputFile = open(outputFileName, 'wb')
-
-# CDS_list = []
-
 locusTag = sys.argv[3]
-
 NtermTrim = float(sys.argv[4])
 CtermTrim = float(sys.argv[5])
 
+outputFileName = "%s.insertions.txt" % sys.argv[3]
+outputFile = open(outputFileName, 'wb')
+
+totalTAsites = []
+totalGenomeLength = []
+TAcoordinates = []
+
 # parse input genbank file with SeqIO
 for sequenceRecord in SeqIO.parse(genbankFile, "genbank"):
+	genome = sequenceRecord.seq
+	genomeSequence = str(sequenceRecord.seq)
+	totalGenomeLength.append(len(genomeSequence))
+	OrganismName = ''.join(sequenceRecord.annotations["source"])
+	totalTAsites.append(genomeSequence.count('TA'))
+		
+	for m in re.finditer('TA', genomeSequence):
+         TAcoordinates.append(m.end())
 	
 	# loop over features in genbank file
 	for feature in sequenceRecord.features:
@@ -62,6 +73,9 @@ for sequenceRecord in SeqIO.parse(genbankFile, "genbank"):
 				matchedLocusTag = ''.join(feature.qualifiers["locus_tag"])
 				product = ''.join(feature.qualifiers["product"])
 				strand = int(feature.location.strand)
+				
+				geneTAcoordinates = []
+				
 				if feature.type == 'CDS':
 					aaLength = len(''.join(feature.qualifiers["translation"]))
 					ntLength = aaLength * 3
@@ -73,18 +87,31 @@ for sequenceRecord in SeqIO.parse(genbankFile, "genbank"):
 					endCoord = int(feature.location.end.position)
 					realStartCoord = int(round(startCoord + ((endCoord - startCoord) * NtermTrim)))
 					realEndCoord = int(round(endCoord - ((endCoord - startCoord) * CtermTrim)))
-					print 'tabulating insertions in %s %s (+ strand, %i nt, %i aa)...' % (locusTag, product, ntLength, aaLength)
+					geneSequence = str(genome[realStartCoord:realEndCoord])
+					gene_TA_count = geneSequence.count('TA')
+					
+					for n in re.finditer('TA', geneSequence):
+						geneTAcoordinates.append(n.end() + realStartCoord)
+					print geneTAcoordinates
+					print 'tabulating insertions in %s - %s (+ strand, %i nt, %i aa, %i TA sites)...' % (locusTag, product, ntLength, aaLength, gene_TA_count)
 				
 				if strand == -1:
 					startCoord = int(feature.location.end.position) #this is not a typo
 					endCoord = int(feature.location.start.position) #this is not a typo
 					realStartCoord = int(round(startCoord - ((startCoord - endCoord) * NtermTrim)))
 					realEndCoord = int(round(endCoord + ((startCoord - endCoord) * CtermTrim)))
-					print 'tabulating insertions in %s %s (- strand, %i nt, %i aa)...' % (locusTag, product, ntLength, aaLength)
+					geneSequence = str(genome[realEndCoord:realStartCoord])
+					rc_geneSequence = str(genome[realEndCoord:realStartCoord].reverse_complement())
+					gene_TA_count = geneSequence.count('TA')
+					
+					for n in re.finditer('TA', geneSequence):
+						geneTAcoordinates.append(n.end() + realEndCoord)
+					#print geneTAcoordinates
+					print 'tabulating insertions in %s - %s (- strand, %i nt, %i aa, %i TA sites)...' % (locusTag, product, ntLength, aaLength, gene_TA_count)
 
+# raise error and exit script if provided locus tag does not exist
 try: matchedLocusTag
 except NameError: matchedLocusTag = None
-
 if matchedLocusTag == None:
 	print 'ERROR: %s does not exist in the provided genome' % locusTag
 	print 'check the locus tag and try again'
@@ -95,31 +122,40 @@ totalSites = []
 for insertionEvent in insertionEvents:
 	if strand and strand == 1:
 		if realStartCoord <= int(insertionEvent[1]) <= realEndCoord:
-			totalSites.append(insertionEvent[1])
 			chromosome = insertionEvent[0]
 			numHits = insertionEvent[2]
 			insertionCoord = int(insertionEvent[1])
-			outputString = "%s\t%s\t%i\t%s" % (chromosome, locusTag, insertionCoord, numHits)
+			if insertionCoord in geneTAcoordinates:
+				totalSites.append(insertionEvent[1])
+				outputString = "%s\t%s\t%i\t%s\tTA site" % (chromosome, locusTag, insertionCoord, numHits)
+			else:
+				outputString = "%s\t%s\t%i\t%s\t*" % (chromosome, locusTag, insertionCoord, numHits)
 			if numHits:
 				print outputString
 				outputFile.write(outputString+"\n")
 		
 	if strand and strand == -1:
 		if realEndCoord <= int(insertionEvent[1]) <= realStartCoord:
-			totalSites.append(insertionEvent[1])
 			chromosome = insertionEvent[0]
 			numHits = insertionEvent[2]
 			insertionCoord = int(insertionEvent[1])
-			outputString = "%s\t%s\t%i\t%s" % (chromosome, locusTag, insertionCoord, numHits)
+			if insertionCoord in geneTAcoordinates:
+				totalSites.append(insertionEvent[1])
+				outputString = "%s\t%s\t%i\t%s\tTA site" % (chromosome, locusTag, insertionCoord, numHits)
+			else:
+				outputString = "%s\t%s\t%i\t%s\t*" % (chromosome, locusTag, insertionCoord, numHits)
 			if numHits:
 				print outputString
 				outputFile.write(outputString+"\n")
 
+print '%s (%i bp) contains %i total theoretical TA insertion sites, of which' % (OrganismName, sum(totalGenomeLength), sum(totalTAsites))
+
 if totalSites:
-	print '%i total unique insertion sites in %s' % (len(totalSites), locusTag)
-	print "insertions written to %s" % outputFileName
+	print '%i total theoretical TA insertion sites are present in %s\n' % (gene_TA_count, locusTag)
+	print 'of these, %i sites in %s (%3.1f %%) were hit' % (len(totalSites), locusTag, (len(totalSites) / float(gene_TA_count)) * 100)
+	print "insertion positions written to %s" % outputFileName
 if not totalSites:
-	print "no transposon insertions in %s" % locusTag
+	print "no transposon insertions occur in %s" % locusTag
 
 outputFile.close()
 genbankFile.close()
